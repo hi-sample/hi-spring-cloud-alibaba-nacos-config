8c682b567e31ccd625d517534a7666db5d5d9faa
ikin <ikin@hifor.net>
2024-09-12 11:56:30 +0800

    update

--- src/main/java/net/hifor/demo/HiSpringCloudAlibabaGradleApplication.java
+++ src/main/java/net/hifor/demo/HiSpringCloudAlibabaGradleApplication.java
@@ -1,13 +1,13 @@
-package net.hifor.configuration;
+package net.hifor.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NacosConfigurationApplication {
+public class HiSpringCloudAlibabaGradleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NacosConfigurationApplication.class, args);
+        SpringApplication.run(HiSpringCloudAlibabaGradleApplication.class, args);
     }
 
 }
